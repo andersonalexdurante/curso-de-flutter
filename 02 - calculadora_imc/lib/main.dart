29888efd203ecb35300d39fe99a9d49c91529bd6
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-09-11 18:04:51 -0300

    SingleChildScrollView e RaisedButton

--- 02 - calculadora_imc/lib/main.dart
+++ 02 - calculadora_imc/lib/main.dart
@@ -29,31 +29,51 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(
-            Icons.person_outline,
-            size: 120,
-            color: Colors.white,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso (kg)",
-                labelStyle: TextStyle(color: Colors.green, fontSize: 20)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura (cm)",
-                labelStyle: TextStyle(color: Colors.green, fontSize: 20)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          )
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(8, 10, 8, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(
+              Icons.person_outline,
+              size: 120,
+              color: Colors.white,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.white, fontSize: 20)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.white, fontSize: 20)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: Container(
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: Colors.green,
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
