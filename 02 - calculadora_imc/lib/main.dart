a468bb3a1ceb2877a8ab780beb59e7c1abd5259b
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-09-11 19:38:10 -0300

    App simples de calculadora de IMC

--- 02 - calculadora_imc/lib/main.dart
+++ 02 - calculadora_imc/lib/main.dart
@@ -24,6 +24,7 @@ class _HomeState extends State<Home> {
     heightController.text = "";
     setState(() {
       _resultText = "Informe seus dados!";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
