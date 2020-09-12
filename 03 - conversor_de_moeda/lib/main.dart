06bad129954922fbe6906ae4b7131a3a303933c9
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-09-12 14:50:51 -0300

    Início do conversor de moedas

---
+++ 03 - conversor_de_moeda/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=18b65089";
+void main() {
+  runApp(MaterialApp(
+    home: Container(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
