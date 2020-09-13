174936f19aa5f08f4b49602df8a27c39387e88c3
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-09-13 15:07:23 -0300

    Refatorando o código em funções

--- 03 - conversor_de_moeda/lib/main.dart
+++ 03 - conversor_de_moeda/lib/main.dart
@@ -22,9 +22,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String value) {
+    print(value);
+  }
+
+  void _dolarChanged(String value) {
+    print(value);
+  }
+
+  void _euroChanged(String value) {
+    print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
           backgroundColor: Colors.yellow,
           centerTitle: true,
@@ -46,11 +66,46 @@ class _HomeState extends State<Home> {
                   child: Text('Erro ao carregar os dados.'),
                 );
               } else {
-                return Container(color: Colors.amber);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150,
+                      ),
+                      buildTextField(
+                          'Reais', 'R\$: ', realController, _realChanged),
+                      Divider(),
+                      buildTextField(
+                          'Dólares', 'US\$: ', dolarController, _dolarChanged),
+                      Divider(),
+                      buildTextField(
+                          'Euros', '£: ', euroController, _euroChanged),
+                    ],
+                  ),
+                );
               }
           }
         },
       ),
+    );
+  }
+
+  Widget buildTextField(String label, String prefix,
+      TextEditingController controller, Function changeValue) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      controller: controller,
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.black45, fontSize: 20),
+          border: OutlineInputBorder(),
+          prefixText: prefix),
+      onChanged: changeValue,
     );
   }
 }
