50e912ad7bc3e5e64874ee40e8f5090630190d14
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-09-13 17:37:05 -0300

    Criando a lista com ListView.Builder

--- 04 - lista_de_tarefas/lib/main.dart
+++ 04 - lista_de_tarefas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -17,10 +16,61 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = [];
+  List _toDoList = ["Andy", "Bebê Sofia"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: Text("Lista de Tarefas"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 5, 0, 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  child: RaisedButton(
+                    shape: CircleBorder(),
+                    color: Colors.blueAccent,
+                    child: Text(
+                      "+",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                      title: Text(_toDoList[index]["title"]),
+                      value: _toDoList[index]["done"],
+                      secondary: CircleAvatar(
+                          child: Icon(
+                        _toDoList[index]["done"] ? Icons.check : Icons.error,
+                      )));
+                }),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
